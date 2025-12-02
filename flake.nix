42b054723a1b870e5da60659bf695997c35942ec
haruki7049 <tontonkirikiri@gmail.com>
2025-12-02 19:35:25 +0900

    Create flakes

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        let
+          nativeBuildInputs = [
+            # nodejs
+            pkgs.nodePackages.nodejs
+
+            # LSP
+            pkgs.nil
+            pkgs.typescript-language-server
+          ];
+        in
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.nixfmt.enable = true;
+            programs.actionlint.enable = true;
+          };
+
+          devShells.default = pkgs.mkShell {
+            inherit nativeBuildInputs;
+
+            shellHook = ''
+              export PS1="\n[nix-shell\w]$ "
+            '';
+          };
+        };
+    };
+}
